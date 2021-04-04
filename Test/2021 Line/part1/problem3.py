e492b5dd3b0911a794aa0fcaa808e3c9b7d8d8fc
chaewon <chechekk0@naver.com>
2021-04-04 22:56:01 +0900

    "0404 코드 정리"

--- Test/2021 Line/part1/problem3.py
+++
@@ -1,6 +0,0 @@
-def solution(enter, leave):
-    answer = []
-
-    return answer
-
-print(solution([1,3,2],[1,2,3]))
--- Test/Nexon Intership/1번.py
+++
@@ -1,5 +0,0 @@
-def addNumbers(a, b):
-    import math
-    return math.floor(a+b)
-
-print(addNumbers(2.3, 1.9))
